5d9fdffb47db69557eaa891976dfb7678beb1d5c
Emre <yunusozudogru34@gmail.com>
2023-09-30 22:10:04 +0200

    part 2 completed!

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,20 +1,43 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user") //request mapping only class level
 public class UserController {
 
+    RoleService roleService;
+    UserService userService;
+
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
     @GetMapping("/create")//endPoint
     public String createUser(Model model){
 
         model.addAttribute("user",new UserDTO());
-        //model.addAttribute("roles",LISTOFROLES); //bring me all roles from DB.
+        model.addAttribute("roles",roleService.findAll());
+        model.addAttribute("users",userService.findAll());
+
         return "/user/create"; //folder path
+    }
+
+    @PostMapping("/create")//endPoint
+    public String insertUser(@ModelAttribute("user") UserDTO user){
+
+       userService.save(user);
+
+
+        return "redirect:/user/create"; //user,roles,users attirbute will be populated end of method
     }
 }
